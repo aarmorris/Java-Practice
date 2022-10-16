c7b0faadf3ff504ba16d71126c1e05fa54037be3
Aaron Morris <aaron.r.morris3@gmail.com>
2022-10-16 16:11:04 -0500

    fourth-commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,23 @@
+import java.awt.*;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        int myAge = 37;
-        int herAge = myAge;
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isEligible = true;
-        Date now = new Date();
-        System.out.println(now);
+//        int myAge = 37;
+//        int herAge = myAge;
+//        long viewsCount = 3_123_456_789L;
+//        float price = 10.99F;
+//        char letter = 'A';
+//        boolean isEligible = true;
+//        Date now = new Date();
+//        System.out.println(now);
+//        Point point1 = new Point(1, 1);
+//        Point point2 = point1;
+//        point1.x = 2;
+//        System.out.println(point2);
+        String message = "    Hello World!" + "!!     ";
+
+        System.out.println(message.trim());
+        System.out.println(message);
     }
 }
