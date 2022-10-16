8d19980e8896170ad8fcbfde1f6a4fa6ac640588
Aaron Morris <aaron.r.morris3@gmail.com>
2022-10-16 15:49:37 -0500

    third-commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int myAge = 37;
+        int herAge = myAge;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = true;
+        Date now = new Date();
+        System.out.println(now);
     }
 }
