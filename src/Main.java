9f18de11ae718cb704170d2743b41caecf7c3eed
Aaron Morris <aaron.r.morris3@gmail.com>
2022-10-17 17:00:40 -0500

    commit-plus

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
 import java.awt.*;
+import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
+            // Older Syntax
+//        int [] numbers = { 2, 3, 5, 1, 4 };
+//        System.out.println(numbers.length);
+//        Arrays.sort(numbers);
+        int [][] numbers = new int [2] [3];
+        numbers [0] [0] = 1;
+        System.out.println(Arrays.deepToString(numbers));
+
+//        numbers[0] = 1;
+//        numbers[1] = 2;
+
+        System.out.println(Arrays.toString(numbers));
 //        int myAge = 37;
 //        int herAge = myAge;
 //        long viewsCount = 3_123_456_789L;
@@ -15,9 +28,8 @@ public class Main {
 //        Point point2 = point1;
 //        point1.x = 2;
 //        System.out.println(point2);
-        String message = "    Hello World!" + "!!     ";
+        String message = "c:\tWindows\\...";
 
-        System.out.println(message.trim());
-        System.out.println(message);
+
     }
 }
