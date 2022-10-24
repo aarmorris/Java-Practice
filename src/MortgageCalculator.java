393ae4841ccc2ffcbded66885df6825a5f3903cc
Aaron Morris <aaron.r.morris3@gmail.com>
2022-10-24 09:01:17 -0500

    mortgage-caclulator

--- src/MortgageCalculator.java
+++ src/MortgageCalculator.java
@@ -14,6 +14,18 @@ public class MortgageCalculator {
 
         System.out.print(("Annual Interest Rate: "));
         float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+        System.out.print("Period (Years): ");
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * MONTHS_IN_YEAR;
+
+        double mortgage = principal
+                    * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                    / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format();
+
 
 
     }
